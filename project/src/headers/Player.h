c38333f8959c8f4575989e16c6f1353171974816
juhtyg <sadovsky11143@gmail.com>
2024-03-07 10:00:49 +0100

    added doc-strings

--- project/src/headers/Player.h
+++ project/src/headers/Player.h
@@ -22,12 +22,43 @@ private:
     std::vector<std::vector<int>>& map;
     bool isLegalPosition(const Vector2d& position);
 public:
+    /**
+     * @brief Construct a new Player object
+     * 
+     * @param texture 
+     * @param position 
+     * @param windowSize 
+     * @param textureSize 
+     * @param map 
+     */
     Player(sf::Texture& texture, Vector2d position, sf::Vector2i windowSize, sf::Vector2u textureSize, std::vector<std::vector<int>>& map);
-
+    
+    /**
+     * @brief Get the Position of the object
+     * 
+     * @return sf::Vector2f 
+     */
     sf::Vector2f getPosition();
+    
+    /**
+     * @brief Get the Direction of the object
+     * 
+     * @return sf::Vector2f 
+     */
     sf::Vector2f getDirection();
 
-    void update(float dt) ;
+    /**
+     * @brief Update the player (check inputs, update position)
+     * 
+     * @param dt - delta time
+     */
+    void update(float dt);
+
+    /**
+     * @brief Render the player and also render the camera(players view)
+     * 
+     * @param window to render
+     */
     void Render(sf::RenderWindow &window) ;
 
 
